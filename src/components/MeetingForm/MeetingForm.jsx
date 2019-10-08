7183f766f6840e6e4fc4fb682348ef93a6020a8b
Dev Govindji <friendsofdev94@gmail.com>
2019-10-08 11:35:03 -0500

    stubs up input functions for adding new items

--- src/components/MeetingForm/MeetingForm.jsx
+++ src/components/MeetingForm/MeetingForm.jsx
@@ -3,43 +3,57 @@ import './MeetingForm.css';
 
 class MeetingForm extends Component {
 
+
+    handleChange = e => {
+        const newDate = { ...this.state.date };
+        newDate[e.target.date] = e.target.value;
+        // console.log(e.target.checkValidity());
+        this.setState({
+            newDate,
+            formInvalid: !this.formRef.current.checkValidity()
+        });
+    };
+
+
+
     render(props) {
         return (
             <div className="MeetingForm">
                 <span>Date<input
                     type="text"
                     value={this.props.date}
-                // onChange={this.handleInput}
+                    onChange={this.handleChange}
+
                 // onKeyPress={this.handleKeyPress}
                 /></span>
 
                 <span>People<input type="text"
                     value={this.props.people}
-                // onChange={this.handleInput} 
-                // onKeyPress={this.handleKeyPress} 
+                    onChange={this.handleChange}
+                // onKeyPress={this.handleKeyPress}
                 /></span>
                 <span>Time<input
                     type="text"
                     value={this.props.time}
-                // onChange={this.handleInput}
+                    onChange={this.handleChange}
                 // onKeyPress={this.handleKeyPress}
                 /></span>
                 <span>Location<input
                     type="text"
                     value={this.props.location}
-                // onChange={this.handleInput}
+                    onChange={this.handleChange}
                 // onKeyPress={this.handleKeyPress}
                 /></span>
                 <span> Notes<input
                     type="text"
                     value={this.props.notes}
-                // onChange={this.handleInput}
+                    onChange={this.handleChange}
                 // onKeyPress={this.handleKeyPress} 
                 />
                 </span>
                 <span><button
                     className=""
-                    onClick={this.createNewToDoItem}
+                    onClick={this.addSchedule}
                 >
                     +
                     </button> </span>
